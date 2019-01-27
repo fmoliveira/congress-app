9217c15d58db1d116a53bf7b9b49ec010ad8d30a
Filipe Oliveira <contato@fmoliveira.com.br>
2019-01-27 01:04:42 -0200

    Create container for content.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Layout from "./components/Header"
+import Layout from "./components/Layout"
 
 const App = () => (
   <Layout>{/* Your app should render this part of the page. */}</Layout>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,28 @@
-import React, { Fragment } from "react"
+import React from "react"
 import { node } from "prop-types"
+import styled, { createGlobalStyle } from "styled-components"
 
 import Header from "../Header"
+import Container from "../Container"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: sans-serif;
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
 
 const Layout = ({ children }) => (
-  <Fragment>
+  <Wrapper>
+    <GlobalStyle />
     <Header />
-    <main>{children}</main>
-  </Fragment>
+    <Container>{children}</Container>
+  </Wrapper>
 )
 
 Layout.propTypes = {
