4c26642926c079fb9fa1941c599b5cb5604048ce
Filipe Oliveira <contato@fmoliveira.com.br>
2019-01-30 11:16:28 -0200

    Setup Redux.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Provider } from "react-redux";
 
 import { Layout, MembersList } from "./components";
 
+import { configureStore } from "./config";
+const store = configureStore();
+
 export const App = () => (
-  <Layout>
-    <MembersList />
-  </Layout>
+  <Provider store={store}>
+    <Layout>
+      <MembersList />
+    </Layout>
+  </Provider>
 );
