43096c408573abedc9ed02f3d189cefece0adf4e
Filipe Oliveira <contato@fmoliveira.com.br>
2019-01-30 11:16:28 -0200

    Setup Redux.

---
+++ src/config/configureStore.ts
@@ -0,0 +1,12 @@
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+import reducers from "../reducers";
+
+export function configureStore() {
+  const middlewares = [thunk];
+  const enhancers = composeWithDevTools(applyMiddleware(...middlewares));
+  const store = createStore(reducers, enhancers);
+  return store;
+}
