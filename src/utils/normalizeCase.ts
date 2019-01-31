dfc1613875e13b659a73199c5ff1b2e2008fdf3f
Filipe Oliveira <contato@fmoliveira.com.br>
2019-01-31 02:30:33 -0200

    Transform all responses to camel case.

---
+++ src/utils/normalizeCase.ts
@@ -0,0 +1,19 @@
+import camelCase from "lodash.camelcase"
+
+export function normalizeCase(anything: any): any {
+  if (Array.isArray(anything)) {
+    return anything.map(normalizeCase)
+  }
+
+  if (typeof anything === "object" && anything !== null) {
+    return Object.keys(anything).reduce(
+      (normalized: any, key) => ({
+        ...normalized,
+        [camelCase(key)]: normalizeCase(anything[key])
+      }),
+      {}
+    )
+  }
+
+  return anything
+}
