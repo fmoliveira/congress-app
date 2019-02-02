8b21840b854c4a6fe4f38e026295efb79eeffedc
Filipe Oliveira <contato@fmoliveira.com.br>
2019-02-02 14:05:05 -0200

    Split skeleton item into a smaller component.

--- src/components/MembersList/SkeletonList.tsx
+++ src/components/MembersList/SkeletonList.tsx
@@ -1,31 +1,6 @@
 import React from "react"
-import styled from "styled-components"
 
-import { CircleSkeleton, TextSkeleton } from "../index"
-
-const ItemWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 2fr repeat(4, 1fr);
-  align-items: center;
-  margin: 0.75rem;
-  padding: 0.5rem;
-  background-color: #ffffff;
-`
-
-const AvatarWrapper = styled.div`
-  display: flex;
-  align-items: center;
-
-  * {
-    margin-right: 0.5em;
-  }
-`
-
-const SocialNetworksWrapper = styled.div`
-  * {
-    margin: 0 4px;
-  }
-`
+import { SkeletonItem } from "./SkeletonItem"
 
 const SKELETON_ITEMS_AMOUNT = 15
 
@@ -34,26 +9,7 @@ const items = new Array(SKELETON_ITEMS_AMOUNT).fill(0).map((_, idx) => idx)
 export const SkeletonList = () => (
   <div>
     {items.map(i => (
-      <ItemWrapper key={i}>
-        <AvatarWrapper>
-          <CircleSkeleton />
-          <TextSkeleton>Member Name</TextSkeleton>
-        </AvatarWrapper>
-        <div>
-          <TextSkeleton>Party Name</TextSkeleton>
-        </div>
-        <div>
-          <TextSkeleton>CA</TextSkeleton>
-        </div>
-        <div>
-          <TextSkeleton>Year</TextSkeleton>
-        </div>
-        <SocialNetworksWrapper>
-          <CircleSkeleton />
-          <CircleSkeleton />
-          <CircleSkeleton />
-        </SocialNetworksWrapper>
-      </ItemWrapper>
+      <SkeletonItem key={i} />
     ))}
   </div>
 )
