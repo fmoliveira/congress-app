1e975b4bd7cce1ef90de7ad9b72fa5f2d67e27c6
Filipe Oliveira <contato@fmoliveira.com.br>
2019-02-02 02:11:19 -0200

    Setup members details store.

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux"
 
+import { membersDetailsStore } from "../components/MemberDetails/reducers"
 import { membersListStore } from "../components/MembersList/reducers"
 import { requestStatusStore } from "./requestStatus"
 
 export default combineReducers({
+  membersDetailsStore,
   membersListStore,
   requestStatusStore
 })
