d2bd494aa7c21762b6c410630b6f03100f86a83d
Filipe Oliveira <contato@fmoliveira.com.br>
2019-02-02 01:30:57 -0200

    Create fallback not found route.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Provider } from "react-redux"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
-import { Layout, MembersList } from "./components"
+import { Layout, MembersList, NotFound } from "./components"
 
 import { configureStore } from "./config"
 const store = configureStore()
@@ -14,6 +14,7 @@ export const App = () => (
         <Switch>
           <Route exact={true} path="/" component={MembersList} />
           <Route path="/list" component={MembersList} />
+          <Route component={NotFound} />
         </Switch>
       </Layout>
     </Router>
