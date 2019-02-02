80d5c36446b1b4cfbea729e719a86e3c4a2b781b
Filipe Oliveira <contato@fmoliveira.com.br>
2019-02-02 02:06:18 -0200

    Create error boundary.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import React from "react"
 import { Provider } from "react-redux"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
-import { Layout, MemberDetails, MembersList, NotFound } from "./components"
+import {
+  ErrorBoundary,
+  Layout,
+  MemberDetails,
+  MembersList,
+  NotFound
+} from "./components"
 
 import { configureStore } from "./config"
 const store = configureStore()
@@ -11,12 +17,14 @@ export const App = () => (
   <Provider store={store}>
     <Router>
       <Layout>
-        <Switch>
-          <Route exact={true} path="/" component={MembersList} />
-          <Route path="/list" component={MembersList} />
-          <Route path="/members/:memberId" component={MemberDetails} />
-          <Route component={NotFound} />
-        </Switch>
+        <ErrorBoundary>
+          <Switch>
+            <Route exact={true} path="/" component={MembersList} />
+            <Route path="/list" component={MembersList} />
+            <Route path="/members/:memberId" component={MemberDetails} />
+            <Route component={NotFound} />
+          </Switch>
+        </ErrorBoundary>
       </Layout>
     </Router>
   </Provider>
