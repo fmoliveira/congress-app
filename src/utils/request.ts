05b00c5bd3410456328a46364e2303310c18603c
Filipe Oliveira <contato@fmoliveira.com.br>
2019-02-02 19:17:50 -0200

    Add support to fetch external urls.

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -6,8 +6,21 @@ const BASE_URL = "https://api.propublica.org/congress/v1"
 
 const API_KEY = process.env.REACT_APP_PROPUBLICA_API_KEY || ""
 
-export function request(relativeUrl: string) {
-  const absoluteUrl = `${BASE_URL}/${relativeUrl}`
+function isAbsoluteUrl(url: string) {
+  const regex = /^http?s:\/\/.*$/
+  return regex.test(url)
+}
+
+function getAbsoluteUrl(url: string) {
+  if (isAbsoluteUrl(url)) {
+    return url
+  }
+  const absoluteUrl = `${BASE_URL}/${url}`
+  return absoluteUrl
+}
+
+export async function request(url: string) {
+  const absoluteUrl = getAbsoluteUrl(url)
   const options = {
     headers: {
       "X-API-Key": API_KEY
