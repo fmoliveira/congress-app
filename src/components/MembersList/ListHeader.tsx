6c8c9332e8a81e99c9e0244486dfa23e1ad01c6e
Filipe Oliveira <contato@fmoliveira.com.br>
2019-02-02 14:10:53 -0200

    Reuse header text from member items.

--- src/components/MembersList/ListHeader.tsx
+++ src/components/MembersList/ListHeader.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import styled from "styled-components"
 
+import { SmallHeader } from "../index"
+
 const HeaderWrapper = styled.div`
   display: grid;
   grid-template-columns: 2fr repeat(4, 1fr);
@@ -8,19 +10,14 @@ const HeaderWrapper = styled.div`
   margin: 0.75rem;
   padding: 0 0.5rem;
   padding-top: 0.5em;
-  font-size: 0.7em;
-  text-transform: uppercase;
-  color: #a6adb4;
 `
-
-const HeaderText = styled.div``
 
 export const ListHeader = () => (
   <HeaderWrapper>
-    <HeaderText>Senators</HeaderText>
-    <HeaderText>Party</HeaderText>
-    <HeaderText>State/District</HeaderText>
-    <HeaderText>Next Election</HeaderText>
-    <HeaderText>Social Networks</HeaderText>
+    <SmallHeader>Senators</SmallHeader>
+    <SmallHeader>Party</SmallHeader>
+    <SmallHeader>State/District</SmallHeader>
+    <SmallHeader>Next Election</SmallHeader>
+    <SmallHeader>Social Networks</SmallHeader>
   </HeaderWrapper>
 )
---
+++ src/components/Text/index.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const SmallHeader = styled.div`
+  font-size: 0.7em;
+  text-transform: uppercase;
+  color: #a6adb4;
+`
