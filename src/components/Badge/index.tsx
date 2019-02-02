483e068fae33afc00f01cd20cc181052e9256a8a
Filipe Oliveira <contato@fmoliveira.com.br>
2019-02-02 20:42:52 -0200

    Display latest news from downloaded rss feed.

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const Badge = styled.span`
+  margin: 0 0.5em;
+  padding: 0.25em 0.5em;
+  font-size: 0.8em;
+  white-space: nowrap;
+  color: #987fb9;
+  background-color: #d0bfe5;
+  border-radius: 10px;
+  text-align: center;
+`
---
+++ src/components/News/FeedItem.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import styled from "styled-components"
+
+import { Badge, Date } from "../index"
+
+interface IProps {
+  feed: any
+}
+
+const ItemWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 100px 1fr;
+  align-items: center;
+  margin: 0.5em 0;
+  color: #888888;
+`
+
+const Link = styled.a`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-size: 0.9em;
+  color: #999999;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+export const FeedItem = ({ feed }: IProps) => (
+  <ItemWrapper>
+    <Badge>
+      <Date value={feed.pubDate} displayFormat="MMM D, YYYY" />
+    </Badge>
+    <Link href={feed.link} target="_blank" rel="noopener noreferrer">
+      {feed.title}
+    </Link>
+  </ItemWrapper>
+)
