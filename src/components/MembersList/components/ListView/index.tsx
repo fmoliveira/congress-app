a88ad480fbb89166ff7655b82781e9d4128b9153
Filipe Oliveira <contato@fmoliveira.com.br>
2019-02-03 22:23:17 -0200

    Change list view header by selected chamber.

--- src/components/MembersList/components/ListView/index.tsx
+++ src/components/MembersList/components/ListView/index.tsx
@@ -12,15 +12,22 @@ import { ListItem } from "./ListItem"
 import { SkeletonList } from "./SkeletonList"
 
 interface IProps {
+  chamber: string
   listMembers: () => void
   members: IMemberInfo[]
   scrollToTop: () => void
   status: RequestStatusType
 }
 
-const ListView = ({ listMembers, members, scrollToTop, status }: IProps) => (
+const ListView = ({
+  chamber,
+  listMembers,
+  members,
+  scrollToTop,
+  status
+}: IProps) => (
   <div>
-    {status !== RequestStatusType.Error && <ListHeader />}
+    {status !== RequestStatusType.Error && <ListHeader chamber={chamber} />}
     {status === RequestStatusType.Error && <ErrorMessage retry={listMembers} />}
     {status === RequestStatusType.Loading && <SkeletonList />}
     {status === RequestStatusType.Success && size(members) === 0 && (
